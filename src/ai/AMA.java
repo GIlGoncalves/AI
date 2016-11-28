6c82360a636c2687c72b98d67d1763d887d1af65
Griffin38 <pedrolima.0000@gmail.com>
2016-11-28 00:32:04 +0000

    Possivel arquitectura
    Agentes nao tÊm behaviours

---
+++ src/ai/AMA.java
@@ -0,0 +1,17 @@
+package ai;
+
+import jade.core.*;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+
+
+
+public class AMA extends Agent {
+	protected void setup(){
+		super.setup();
+		//this.addBehaviour(new SendMessage());
+		//this.addBehaviour(new ReceiveBehaviour());
+	}
+	
+}
---
+++ src/ai/APHist.java
@@ -0,0 +1,17 @@
+package ai;
+
+import jade.core.*;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+
+
+
+public class APHist extends Agent {
+	protected void setup(){
+		super.setup();
+		//this.addBehaviour(new SendMessage());
+		//this.addBehaviour(new ReceiveBehaviour());
+	}
+	
+}
